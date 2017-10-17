84d70923499b2f9d215099e3876c48a34a1e2e24
Kunal Basu <kunal.basu@berkeley.edu>
2017-10-16 19:44:37 -0700

    migration

--- schedule_builder/models.py
+++ schedule_builder/models.py
@@ -4,12 +4,19 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length = 50)
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, default='')
+
+    def __str__(self):
+        return self.name
 
 class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
     posted = models.DateTimeField(db_index=True, auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
 
     def __unicode__(self):
         return '%s' % self.title
@@ -18,5 +25,3 @@ class Blog(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('view_blog_post', None, { 'slug': self.slug })
-
-
